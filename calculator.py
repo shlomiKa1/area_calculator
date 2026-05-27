d1b8a5578c0d1703e6c1dc0b19a88b2e8972c960
Shlomi <shlomikaufman91@gmail.com>
2026-05-27 16:24:06 +0300

    Add class calculator

---
+++ calculator.py
@@ -0,0 +1,12 @@
+class Shape:
+    def __init__(self, shape_type):
+        self.shape_type = shape_type
+
+    def get_area(self):
+        pass
+
+    def get_perimeter(self):
+        pass
+    
+    def __str__(self):
+        pass
